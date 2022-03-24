6821b811060fb5587945c6c614e2dbd187002cc4
JessicaMiguel <miguel.jessica.e@gmail.com>
2022-03-24 19:17:31 -0300

    Initial commit

---
+++ src/main/java/com/example/demo/service/UserServiceImplementation.java
@@ -0,0 +1,45 @@
+package com.example.demo.service;
+
+import com.example.demo.domain.Role;
+import com.example.demo.domain.User;
+import com.example.demo.repository.RoleRepository;
+import com.example.demo.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service@RequiredArgsConstructor @Transactional @Slf4j
+public class UserServiceImplementation implements UserService{
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+
+    @Override
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public Role saveRole(Role role) {
+        return roleRepository.save(role);
+    }
+
+    @Override
+    public void addRoleToUser(String username, String roleName) {
+        User user = userRepository.findByUsername(username);
+        Role role = roleRepository.findByName(roleName);
+        user.getRoles().add(role);
+    }
+
+    @Override
+    public User getUser(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+}
